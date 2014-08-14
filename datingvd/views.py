26f2078af8321fc2f87a97dcf1f27b7d6324ddf1
Amit Juneja <amit.juneja004@gmail.com>
2014-08-14 01:15:05 -0700

    main page added 3

--- datingvd/views.py
+++ datingvd/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 
 def index(request):
-	HttpResponse('<h1>Main Page</h1>')
+	return HttpResponse('<h1>Main Page</h1>')
 
-	
